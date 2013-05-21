bd90b36370add3ff460134363600cd165f53394c
Pooshonk <shaun.brown@peacockcarter.co.uk>
2013-05-21 17:05:27 +0100

    Fixed git ignore.

---
+++ wp-content/plugins/weeb-related-posts/includes/classes/weeb_related_posts.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * weeb_related_posts
+ *
+ *
+ * @author     John Richardson <johnrich85@hotmail.com>
+ */
+
+class weeb_related_posts {
+
+    //Class properties
+    private $num_posts;
+    private $num_required;
+    private $posts;
+    private $query_args;
+    private $related_query;
+
+    public function __construct($num_required = 2) {
+
+        $this->num_required = $num_required;
+
+    }
+
+    /**
+     *
+     * Recursive function - repeats x amount of times until the required
+     * number of posts are found, or the search criteria can no longer
+     * be dimished.
+     *
+     * @return mixed
+     */
+
+    public function getRelatedPosts() {
+
+        //Run query.
+        $this->related_query = new WP_Query($this->query_args);
+
+        while ( $this->related_query->have_posts()  ) {
+            $this->related_query->the_post();
+
+            //Store posts to class property.
+            $post = array();
+            $post['title'] = get_the_title();
+            $post['url'] = get_permalink();
+            $post['image'] = get_the_post_thumbnail(get_the_ID());
+
+            //Keeping track of the number of posts matched.
+            $this->num_posts ++;
+
+            //Maximum posts to be returned by next query.
+            $this->query_args['posts_per_page'] -= $this->num_posts;
+
+            //Keeping track of the exact posts matched, so that they are not returned more than once.
+            $this->store_ID(get_the_ID());
+
+            //Storing post details in array.
+            $this->posts[] = $post;
+
+        }
+
+        //Check if enough posts returned
+        if ($this->num_posts < $this->num_required && $this->diminish_search_params()) {
+            return $this->getRelatedPosts();
+        }
+        else {
+
+            return $this->posts;
+        }
+
+    }
+
+    /**
+     *
+     * Makes the search parameters less restrictive on each call. Returns false
+     * if this is not possible, which can be used to stop the recursion in
+     * 'getRelatedPosts()'
+     *
+     * @return bool
+     */
+
+    public function diminish_search_params() {
+
+        //No longer restricting results by title
+        if ( isset($this->query_args['search_prod_title'])) {
+            unset($this->query_args['search_prod_title']);
+            return true;
+        }
+
+        //No longer restricting results by category
+        if ( isset($this->query_args['category__in'])) {
+            unset($this->query_args['category__in']);
+            return true;
+        }
+
+        return false;
+
+    }
+
+    /**
+     *
+     * Used to assign search parameters to class property.
+     *
+     * @param $argsArray Array containing parameters for WP_Query
+     */
+
+    public function set_query_args($argsArray) {
+        //Defining arguments for wp_query.
+        $this->query_args = $argsArray;
+    }
+
+    /**
+     *
+     * Returns the number of posts currently stored in $this->posts
+     *
+     * @return int
+     */
+
+    public function getNumPosts() {
+        return $this->num_posts;
+    }
+
+    /**
+     *
+     * Adds an ID to the ignore list so that it will not be returned
+     * in future queries.
+     *
+     * @param $id post id
+     */
+
+    public function store_ID($id) {
+        if ( isset($this->query_args['post__not_in']) ) {
+            $this->query_args['post__not_in'][] = $id;
+        }
+        else {
+            $this->query_args['post__not_in'] = array();
+            $this->query_args['post__not_in'][] = $id;
+        }
+    }
+
+
+
+}
---
+++ wp-content/plugins/weeb-related-posts/weeb-related-posts.php
@@ -0,0 +1,94 @@
+<?php
+/*
+Plugin Name: Weeb related posts
+Plugin URI: http://weebtutorials.com
+Description: A simple related posts plugin made for demonstration purposes.
+Version: 1.0
+Author: John Richardson
+Author URI: http://weebtutorials.com
+License: GPL2
+
+    Copyright 2013  John Richardson  (email : johnrich85@hotmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+//Defining Constants
+define( 'PLUGIN_DIR', dirname(__FILE__).'/' );
+
+
+//Hooks a function to a filter action, 'the_content' being the action, 'hello_world' the function.
+add_filter('the_content','related_posts');
+
+//Callback function
+function related_posts($content)
+{
+    //Gives us access to the global post object.
+    global $post;
+
+	//Checking if on post page.
+	if ( is_single() ) {
+
+        //Including classes
+        include(PLUGIN_DIR."includes/classes/weeb_relatable_properties.php");
+        include(PLUGIN_DIR."includes/classes/weeb_related_posts.php");
+        include(PLUGIN_DIR."includes/classes/weeb_related_posts_factory.php");
+
+        //Other includes
+        include(PLUGIN_DIR."includes/weeb_functions.php");
+
+        //Using a factory to abstract away the process of instantiating related posts object.
+        $getRelatedPosts = weeb_related_posts_factory::create($post, 2);
+
+        //Also want to filter results by title - need to add filter for this
+        add_filter( 'posts_where', 'title_filter', 10, 2 );
+
+        //Fetching the related posts.
+        $related = $getRelatedPosts->getRelatedPosts();
+
+        //Remove filter, won't be using it again.
+        remove_filter('posts_where', 'title_filter');
+
+        //Checking for related posts.
+        if ( count($related) > 0 ) {
+            //Add the stylesheet.
+            wp_enqueue_style( 'myPluginStylesheet', plugins_url('css/weeb_related_posts.css', __FILE__)  );
+
+            $rel_html = '<h3 id="weeb-related-title"> You may also like these posts: </h3>';
+            $rel_html .= "<ul id=\"weeb-related-posts\">";
+            foreach ( $related as $the_post) {
+                $rel_html .= "<li>";
+                    $rel_html .= "<a href=\"".$the_post['url'] ."\">";
+                    $rel_html .= $the_post['image'];
+                    $rel_html .= "<h4> ".$the_post['title']."</h4>";
+                    $rel_html .= "</a>";
+                $rel_html .= "</li>";
+            }
+            $rel_html .= "</ul>";
+
+
+            //Adding custom content to end of post.
+            return $content . $rel_html;
+        }
+        else {
+            return $content;
+        }
+	}
+	else {
+		//else on blog page / home page etc, just return content as usual.
+		return $content;
+	}
+}
+
+?>
